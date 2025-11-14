2cf56ccb9a4eb3250bbcaece4e29933d55066fad
cloudymike <mikael@cloudyforecast.com>
2025-11-14 11:11:35 -0800

    First created

--- PJ320/commandline.sh
+++ PJ320/commandline.sh
@@ -22,13 +22,6 @@ pcb2gcode \
   --outline Gerber_BoardOutlineLayer.GKO --outline-output Gerber_BoardOutlineLayer.nc
 fi
 
-# Create a file that can be used to engrave the board outline for test
-# Does it two rounds, not required but smarter hack needed to fix that.
-echo "Create board outline scratch"
-sed 's/G01 Z-.* F25.00000 ( plunge. )/G01 Z-0.01000 F25.00000 ( plunge. )/g' \
-  < Gerber_BoardOutlineLayer.nc \
-  > Scratch_BoardOutline.nc
-
 # Cleanup, may be too much 
 rm -f *.svg
 rm -f *.GT?
